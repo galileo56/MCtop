5bca852a54db2562fc6f57eab873041e89b5bf2f
Vicent <vmateu@usal.es>
2017-02-15 13:44:05 +0100

    minimal

--- src/Legendre.F90
+++ src/Legendre.F90
@@ -14,12 +14,12 @@ program Legendre
 
   allocate( list(2,0:n) ); moQ = (mt/Q)**2; Cmax = 12 * moQ * (1 - 3 * moQ)
 
-  print*, mt, mW, Q; print*,
+  print*, mt, mW, Q, mt/Q; print*,
 
   call f90CparamLegendre(n, mt, 0._dp, mW, Q, 'noexpand', method, spin, 'unstable', &
                          current, 0._dp, Cmax, Nevent, Niter, list)
   do i = 0, n
-    print*, i, list(:,i)
+    write( *, '(I2,5F18.6)' ) i, list(:,i)
   end do
 
   deallocate(list)
